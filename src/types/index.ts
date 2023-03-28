cdb6e28765a68484a41a69195a47b8df2ca7ca47
Alwoch Sophia <salwoch@gmail.com>
2023-03-28 11:56:53 +0300

    Collections (#8)
    * feat(collection): create momo product abstract class, add momo client option types
    
    * feat(collection): add collection class
    
    * chore(setup): add node-fetch

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 export type BaseUrlCategory = 'API' | 'PAYMENTS'
 
+export type XTargetEnvironment = 'sandbox' | 'live'
+
 export type EndpointCategory =
   | 'CREATE_SUBSCRIPTION'
   | 'FETCH_SUBSCRIPTION'
@@ -15,3 +17,19 @@ export type Endpoints = {
     readonly endpoint: string
   }
 }
+
+export interface CreateAccessToken {
+  readonly expires_in: number
+  readonly access_token: string
+  readonly token_type: string
+ }
+
+export interface MomoClientOptions {
+  readonly 'X-Target-Environment': XTargetEnvironment
+  readonly 'Ocp-Apim-Subscription-Key': string
+  readonly Authorization?: string
+  readonly 'API-Key': string
+  readonly 'X-Reference-Id': string
+}
+
+export interface ICollection{}
