684203116b7886b7decc8558c12c2bee0a7576e4
Alwoch Sophia <salwoch@gmail.com>
2023-04-07 10:47:11 +0300

    feat(collection): Add requestToPayTransactionStatus (#12)
    * feat(collection): create momo product abstract class, add momo client option types
    
    * feat(collection): add collection class
    
    * chore(setup): add node-fetch
    
    * chore(setup): refactor build script, set up tsup config
    
    * feat(collection): add request to pay
    
    * feat(collection): Refactor request to pay to include error handling
    
    * feat(collection): Add requestToPayTransactionStatus

--- src/MomoProducts/collection/endpoints.ts
+++ src/MomoProducts/collection/endpoints.ts
@@ -1,3 +1,4 @@
 export enum CollectionEndPoints{
- REQUEST_TO_PAY="v1_0/requesttopay"
+ REQUEST_TO_PAY="v1_0/requesttopay",
+ REQUEST_TO_PAY_TRANSACTION_STATUS="v1_0/requesttopay"
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -22,55 +22,69 @@ export interface CreateAccessToken {
   readonly expires_in: number
   readonly access_token: string
   readonly token_type: string
- }
+}
 
 export interface MomoClientOptions {
   readonly 'X-Target-Environment': XTargetEnvironment
   readonly 'Ocp-Apim-Subscription-Key': string
   readonly 'API-Key': string
   readonly 'X-Reference-Id': string
-  readonly 'X-Callback-Url'?:string | undefined
+  readonly 'X-Callback-Url'?: string | undefined
 }
 
-export interface RequestToPayOptions{
-  readonly 'amount':string
-  readonly "currency":string
-  readonly "externalId":string
-  readonly "payer":{
-    readonly "partyIdType":'MSISDN' | 'EMAIL' | 'PARTY_CODE'
-    readonly "partyId":string
+export interface RequestToPayOptions {
+  readonly amount: string
+  readonly currency: string
+  readonly externalId: string
+  readonly payer: {
+    readonly partyIdType: 'MSISDN' | 'EMAIL' | 'PARTY_CODE'
+    readonly partyId: string
   }
-  readonly "payerMessage":string
-  readonly "payeeNote":string
+  readonly payerMessage: string
+  readonly payeeNote: string
 }
 
-interface RequestToPayData {
-  readonly status_code: number;
-  readonly message: string;
-  readonly referenceId: string;
-};
+export type TransactionStatus =
+  | 'PENDING'
+  | 'SUCCESSFUL'
+  | 'REJECTED'
+  | 'TIMEOUT'
+  | 'FAILED'
 
-interface MomoError{
-  readonly status_code: number;
-  readonly message: string;
-};
+export interface RequestToPayTransactionStatus extends RequestToPayOptions {
+  readonly status: TransactionStatus
+}
 
-export interface RequestToPayResponse {
-  readonly data: RequestToPayData | null;
-  readonly error: MomoError | null;
-};
+interface MomoError {
+  readonly status_code: number
+  readonly status_text: string
+  readonly message: string
+}
+
+export interface RequestToPayData {
+  readonly status_code: number
+  readonly message: string
+  readonly referenceId: string
+}
+
+export interface MomoResponse<T> {
+  readonly data: T | null
+  readonly error: MomoError | null
+}
 
 export interface RequestToPayHeaders {
-  readonly [header: string]: string | undefined;
-  readonly "Content-Type": string;
-  readonly "Ocp-Apim-Subscription-Key": string;
-  readonly "X-Reference-Id": string;
-  readonly "X-Target-Environment": XTargetEnvironment;
-  readonly "Authorization": string;
-  readonly "X-Callback-Url"?: string;
+  readonly [header: string]: string | undefined
+  readonly 'Content-Type': string
+  readonly 'Ocp-Apim-Subscription-Key': string
+  readonly 'X-Reference-Id': string
+  readonly 'X-Target-Environment': XTargetEnvironment
+  readonly Authorization: string
+  readonly 'X-Callback-Url'?: string
 }
 
-
 export interface ICollection{
-  requestToPay(options:RequestToPayOptions):Promise<RequestToPayResponse>
+  requestToPay(options: RequestToPayOptions): Promise<MomoResponse<RequestToPayData>>
+  requestToPayTransactionStatus(
+    referenceId: string
+  ): Promise<MomoResponse<RequestToPayTransactionStatus>>
 }
