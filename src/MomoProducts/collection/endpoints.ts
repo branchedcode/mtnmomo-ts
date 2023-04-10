f3ef70680982fe84a98a9e34fa0d66b8e849e648
Alwoch <salwoch@gmail.com>
2023-04-10 12:22:02 +0300

    feat(collection): add validateAccountHolderStatus

--- src/MomoProducts/collection/endpoints.ts
+++ src/MomoProducts/collection/endpoints.ts
@@ -2,5 +2,6 @@ export enum CollectionEndPoints {
   REQUEST_TO_PAY = 'v1_0/requesttopay',
   REQUEST_TO_PAY_TRANSACTION_STATUS = 'v1_0/requesttopay',
   GET_BASIC_USER_INFO = 'v1_0/accountholder/msisdn',
-  GET_ACCOUNT_BALANCE='v1_0/account/balance'
+  GET_ACCOUNT_BALANCE='v1_0/account/balance',
+  VALIDATE_ACCOUNT_HOLDER_STATUS='v1_0/accountholder'
 }
